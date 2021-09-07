22d063fd6b0e9df6caa02eae5ca244f0c2886365
Randi Firmansyah <randykelvin26@gmail.com>
2021-09-07 17:16:22 +0700

    animasi loading

---
+++ src/components/atom/Loading.js
@@ -0,0 +1,16 @@
+import Loader from 'react-loader-spinner'
+
+const FirstLoading = () => {
+  return (
+    <div className="container">
+      <div
+        className="d-flex justify-content-center"
+        style={{ paddingTop: '25%' }}
+      >
+        <Loader type="Bars" color="#112031" height="100" width="100" />
+      </div>
+    </div>
+  )
+}
+
+export default FirstLoading
